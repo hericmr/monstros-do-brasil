068be71e125cd52f66959774b53b3a94bfa4aba9
hericmr <seu-email@dominio.com>
2025-02-25 08:36:25 -0300

    ipupiara

--- src/components/detalhesInfo.js
+++ src/components/detalhesInfo.js
@@ -5,18 +5,16 @@ const detalhesIntro = {
           <p class="mb-4">Esta é uma cybercartografia dos seres sobrenaturais e mágicos do Brasil, uma representação visual que reúne os mitos e criaturas encantadas que povoam o país, indicando seus locais de origem. Os seres místicos que habitam o Brasil são essenciais para a compreensão do patrimônio cultural nacional, refletindo a pluralidade de influências indígenas, africanas e europeias que moldaram o imaginário popular brasileiro.
 
  Assim, este mapa não só serve como ferramenta didática, mas também convida à reflexão crítica e ao engajamento na preservação dos mitos ancestrais que compõem o fascinante universo das entidades mágicas e monstruosas do Brasil.</p>
-          
+
           <div class="border border-blue-200 p-4 rounded-lg bg-blue-100 max-w-md mx-auto">
             <center><ul class="list-none space-y-2">
               <li class="flex items-center">
-                <img src="public/fotos/Captura_de_imagem_20250224_124603-removebg-preview.png" alt="Marcador Azul" class="w-5 h-8 mr-2" />
+                <img src="https://github.com/hericmr/monstros-do-brasil/blob/main/public/fotos/Captura_de_imagem_20250224_124603-removebg-preview.png?raw=true" alt="Marcador Azul" class="w-10 h-16 mr-2" />
                 <span><strong class="text-blue-700">Ipupiara:</strong> Monstro de São Vicente.</span>
               </li>
-
-          
-          
+            </ul></center>
+          </div>
         `,
 };
-
 
 export default detalhesIntro;
