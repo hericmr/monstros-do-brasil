589ff41a69576fcd229d6200ef0503e9f8cf5751
hericmr <seu-email@dominio.com>
2025-02-25 07:37:39 -0300

    ipupiara

--- src/components/CustomIcon.js
+++ src/components/CustomIcon.js
@@ -1,22 +1,29 @@
 import L from "leaflet";
 
-// Cria uma animação de brilho e pulsação customizada
 const createIcon = (url, color) =>
   new L.DivIcon({
-    className: `group`, // Remove a classe de animação
+    className: `monster-icon`,
     html: `
-      <div class="relative">
-        <img src="${url}" class="w-[20px] h-[33px] drop-shadow-md">
-
+      <div class="relative animate-[monster-pulse_2s_infinite]">
+        <img src="${url}" 
+             class="w-[40px] h-[66px] 
+                    border-2 border-blue-900 
+                    rounded-full 
+                    rotate-[20deg]
+                    transition-all
+                    [filter:drop-shadow(0_0_8px_#0066ff)_drop-shadow(0_0_3px_#0066ff)]">
+        <div class="absolute inset-0 bg-blue-400 rounded-full mix-blend-screen opacity-30"></div>
       </div>
     `,
-    iconSize: [10, 17],
-    iconAnchor: [9, 30],
-    popupAnchor: [1, -14],
+    iconSize: [40, 66],
+    iconAnchor: [20, 66],
+    popupAnchor: [0, -70],
   });
 
-// Criando ícones com cores personalizadas
-export const blueIcon = createIcon("public/fotos/Captura_de_imagem_20250224_124603-removebg-preview.png", "blue");
+export const blueIcon = createIcon(
+  "https://github.com/hericmr/monstros-do-brasil/blob/main/public/fotos/Captura_de_imagem_20250224_124603-removebg-preview.png?raw=true",
+  "blue"
+);
 export const greenIcon = createIcon("https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png", "green");
 export const yellowIcon = createIcon("https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png", "yellow");
 export const redIcon = createIcon("https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png", "red");
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Caminho para os arquivos React
   ],
+
   theme: {
-    extend: {}, // Personalizações, caso necessário
+    extend: {
+      keyframes: {
+        'monster-pulse': {
+          '0%, 100%': { transform: 'scale(0.95)', opacity: '0.8' },
+          '70%': { transform: 'scale(1.1)', opacity: '1' },
+        },
+      },
+      animation: {
+        'monster-pulse': 'monster-pulse 2s infinite',
+      },
+    },
   },
   plugins: [],
-};
+}
